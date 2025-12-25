9699a3ea6a52e627297b5c2826f05ae95f84b375
arya_ganteng <sitialysa405@example.com>
2025-12-25 16:12:59 +0700

    feat: complete shop registration flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/admin/Dashboard';
+import OpenShopPage from './pages/admin/OpenShopPage';
 
 function App() {
   return (
